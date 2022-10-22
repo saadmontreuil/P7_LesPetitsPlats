0307fdbe1bc8b58b19ee1e881c4ae1b70b2e612a
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2022-10-22 13:33:40 +0200

    Show filtered recipes By searchBar input

--- scripts/Recipe.js
+++ scripts/Recipe.js
@@ -4,16 +4,21 @@ import { recipes } from "../data/recipes.js";
 let recipeArray = [];
 let recipeHTML = "";
 const recipesContainer = document.getElementById("recipes");
+const searchPanel = document.querySelector('#search-panel')
+const recipess = document.getElementsByClassName("recipes");
+let searchValue = ''
 
 //fonction Affichage des recettes sur la page
 const getRecipes=(recipes)=> {
+  
+
   recipeArray = recipes.map((recipe) => {
     return {
       recipe: recipe,
       ingredients: recipe.ingredients.map(itemIngridient),
       html: `
       <div class="recipe">
-      <img class="recipeImg" src="https://picsum.photos/380/178" alt="recipe">
+      <img class="recipeImg" src="https://picsum.photos/380/178?grayscale&blur" alt="recipe">
       <div class="recipeWrap">
           <div class="recipeHeader">
               <h2 class="recipeTitle">${recipe.name}</h2>
@@ -40,6 +45,7 @@ const getRecipes=(recipes)=> {
   recipeArray.forEach((element) => {
     recipeHTML += element.html; 
   });
+  recipess.innerHTML = "";
   recipesContainer.innerHTML = recipeHTML;
 
 }
@@ -53,5 +59,22 @@ const getIngredients=(item)=>  {
     item.quantity || ""
   } ${item.unit || ""} <br>`;
 }
+
 //Affichage des recettes sur la page
 getRecipes(recipes);
+
+const search = () => {
+  const searchbarFilter = recipes.filter(({name,ingredients,description}) => JSON.stringify({name,ingredients,description}).toLowerCase().includes(searchValue));
+  console.log(searchbarFilter);
+  
+   getRecipes(searchbarFilter)
+}
+
+searchPanel.addEventListener('submit', (event) => {
+  event.preventDefault()
+  searchValue = Object.fromEntries(new FormData(event.target)).search.trim().toLowerCase()
+  console.log(searchValue);
+  if (searchValue.length > 2) {
+      search(searchValue)
+  }
+})
